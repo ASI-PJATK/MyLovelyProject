592f98742a602d40f474256bdb54fd9c224b7651
Dawid Kazubski <dawid.kazubski@gmail.com>
2023-10-28 15:57:06 +0200

    package with working module

---
+++ python-packages/src/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    name="asi-project",
+    version="0.0.1",
+    description="Python package for asi project",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
