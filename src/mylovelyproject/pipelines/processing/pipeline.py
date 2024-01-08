e09302f38079e0155b464c322998f8577ceeb02f
Duzduran <ahmetduzduran@gmail.com>
2024-01-08 01:28:18 +0100

    removed cat feature

--- src/mylovelyproject/pipelines/processing/pipeline.py
+++ src/mylovelyproject/pipelines/processing/pipeline.py
@@ -22,28 +22,23 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs="explored_housing_data",
                 name="explore_housing_data_node"
             ),
-            node(
-                func=stratify_data,
-                inputs="explored_housing_data",
-                outputs=["strat_train_set", "strat_test_set"],
-                name="stratify_data_node"
-            ),
-            node(
-                func=add_features,
-                inputs="strat_train_set",
-                outputs="feature_added_housing",
-                name="add_features_node"
-            ),
+
+            # node(
+            #     func=add_features,
+            #     inputs="explored_housing_data",
+            #     outputs="feature_added_housing",
+            #     name="add_features_node"
+            # ),
             node(
                 func=prepare_data,
-                inputs="feature_added_housing",
-                outputs=["housing_prepared", "housing_labels"],
+                inputs="explored_housing_data",
+                outputs=["X_train", "y_train", "X_test", "y_test"],
                 name="prepare_data_node"
             ),
 
             node(
                 func=train_model_with_pycaret,  # train_model for normal train
-                inputs=["housing_prepared", "housing_labels"],
+                inputs=["X_train", "y_train"],
                 outputs="best_model",  # change it to trained_model in case of normal train
                 name="train_model_with_pycaret_node"  # train_model_node for normal train
             ),
@@ -55,9 +50,15 @@ def create_pipeline(**kwargs) -> Pipeline:
             ),
             node(
                 func=predict_pycaret,  # predict for normal train
-                inputs=["tuned_model", "housing_prepared"],  # 'best_model' for normal train
+                inputs=["tuned_model", "X_test"],  # 'best_model' for normal train
                 outputs="predictions",
                 name="predict_node"
+            ),
+            node(
+                func=features_to_dataframe,
+                inputs="X_train",
+                outputs="housing_feature_df",
+                name="convert_feature_node"
             ),
             node(
                 func=predictions_to_dataframe,
